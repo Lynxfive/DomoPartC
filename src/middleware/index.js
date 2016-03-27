51481dfea1b287b5bb9da8705f8ae8647a6bd321
Lynxfive <cjc6417@rit.edu>
2016-03-27 18:03:48 -0400

    Found the mistake I think

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -8,7 +8,7 @@ var requiresLogin = function(req, res, next) {
 
 var requiresLogout = function(req, res, next) {
 	
-	if(!req.session.account){
+	if(req.session.account){
 		return res.redirect('/maker');
 	}
 	next();
